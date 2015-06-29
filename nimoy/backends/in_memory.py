14667cffa5f721cc12a8e13d6e66e731a7def635
Andrási László <laszlo.andrasi@lensa.com>
2015-06-29 10:35:27 +0200

    in_memory empty get

--- nimoy/backends/in_memory.py
+++ nimoy/backends/in_memory.py
@@ -1,4 +1,5 @@
 from .base import BaseBackend
+from nimoy.exceptions import ItemNotFound
 
 _global_state = {}
 
@@ -21,7 +22,10 @@ class InMemoryBackend(BaseBackend):
         return True
 
     def get_item(self, schema_name, _id):
-        return _global_state[schema_name].get(_id)
+        try:
+            return _global_state[schema_name].get(_id)
+        except KeyError:
+            raise ItemNotFound("Item not found for id {} in {}.".format(_id, schema_name))
 
     def delete_item(self, schema_name, _id):
         if _id in _global_state[schema_name]:
