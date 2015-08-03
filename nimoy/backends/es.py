7ed78650bb321a59d9919f61c7a6a82dc36dc0ed
Andrási László <laszlo.andrasi@lensa.com>
2015-08-03 11:55:38 +0200

    0.7.3 es query, count, scan first try

--- nimoy/backends/es.py
+++ nimoy/backends/es.py
@@ -46,10 +46,75 @@ class ElasticsearchBackend(BaseBackend):
         return result['found'] is True
 
     def query(self, schema_name, _w, limit=10):
-        raise NotImplemented
+        return self.scan(schema_name, _w, limit)
 
     def scan(self, schema_name, _w, limit=10):
-        raise NotImplemented
+        query = elastic_parse_wt(_w, {})
+        query["size"] = limit
+        result = self._es.search(index=schema_name, doc_type=schema_name, body=query)
+        return [hit['_source'] for hit in result["hits"]["hits"]]
 
     def query_count(self, schema_name, _w):
-        raise NotImplemented
+        query = elastic_parse_wt(_w, {})
+        result = self._es.count(index=schema_name, doc_type=schema_name, body=query)
+        return result.get('count', 0)
+
+
+def elastic_parse_wt(wt, fmap):
+
+    def _es_match_all(r, f, v):
+        return {"match_all": {}}
+
+    def _get_fn_from_fmap(f):
+        return fmap.get(f, lambda v: v)
+
+    def _es_nin_rel(r, f, v):
+        return {"not": {"terms": {f: [_get_fn_from_fmap(f)(v_) for v_ in v]}}}
+
+    def _es_in_rel(r, f, v):
+        return {"terms": {f: [_get_fn_from_fmap(f)(v_) for v_ in v]}}
+
+    def _es_contains_rel(r, f, v):
+        return _es_in_rel("in", f, [v])
+
+    def _es_ncontains_rel(r, f, v):
+        return _es_nin_rel("nin", f, [v])
+
+    def _es_range_rel(r, f, v):
+        return {"range": {f: {r: _get_fn_from_fmap(f)(v)}}}
+
+    def _es_missing_rel(r, f, v):
+        return {"missing": {"field": f}}
+
+    def _es_eq_rel(r, f, v):
+        return {"term": {f: _get_fn_from_fmap(f)(v)}}
+
+    def _es_neq_rel(r, f, v):
+        return {"not": {"term": {f: _get_fn_from_fmap(f)(v)}}}
+
+    def _es_startswith(r, f, v):
+        return {"regexp": {f: "{}.*".format(v)}}
+
+    def _es_match(r, f, v):
+        return _es_eq_rel(r, "{}.ngrams".format(f), v)
+
+    es_simple_relation_map = {"from": _es_range_rel, "to": _es_range_rel,
+                              "gte": _es_range_rel, "lte": _es_range_rel,
+                              "gt": _es_range_rel, "lt": _es_range_rel,
+                              "in": _es_in_rel, "nin": _es_nin_rel,
+                              "missing": _es_missing_rel,
+                              "contains": _es_contains_rel, "ncontains": _es_ncontains_rel,
+                              "eq": _es_eq_rel, "neq": _es_neq_rel,
+                              "startswith": _es_startswith,
+                              "match": _es_match,
+                              "match_all": _es_match_all}
+
+    def _filters(wt, fmap):
+        if wt[0] in ["and", "or"]:
+            ret = {wt[0]: [_filters(x, fmap) for x in wt[1:]]}
+        else:
+            (relation, field, value) = wt
+            ret = es_simple_relation_map[relation](*wt)
+        return ret
+
+    return {"filter": _filters(wt, fmap)}
